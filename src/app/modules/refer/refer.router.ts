b86194418da07e6b50cbad40c0ce3784fac73687
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-07-06 13:05:46 +0530

    changes

--- src/app/modules/refer/refer.router.ts
+++ src/app/modules/refer/refer.router.ts
@@ -4,7 +4,9 @@ import { ReferController } from "./refer.controller";
 const router = express.Router();
 
 router.post("/", ReferController.createRefer);
+
 router.get("/", ReferController.getRefers);
+
 router.get("/:id", ReferController.getRefer);
 
 export const ReferRouters = router;
