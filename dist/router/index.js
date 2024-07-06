89920f520fb631ae513231ae04ac294cbeb70efd
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-07-06 12:07:38 +0530

    changes

---
+++ dist/router/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const refer_router_1 = require("../app/modules/refer/refer.router");
+const router = express_1.default.Router();
+const routers = [
+    {
+        path: "/refer",
+        route: refer_router_1.ReferRouters,
+    },
+];
+routers.forEach((route) => router.use(route.path, route.route));
+exports.default = router;
